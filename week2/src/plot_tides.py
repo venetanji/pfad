c7a3358a8316d9b72e5c347fb14b00be99dc1204
Snowking <gyk16@tsinghua.org.cn>
2024-09-23 22:51:43 +0800

    Changes

--- week2/src/plot_tides.py
+++ week2/src/plot_tides.py
@@ -1,58 +1,32 @@
-import requests
-from lxml import html
-
-import dotenv
-import os
-import datetime
 import matplotlib.pyplot as plt
-from scraping_utils import get_url, parse
+import pandas as pd
+import chardet
 
-# load the environment variables
-dotenv.load_dotenv('../../week1/src/.env')
 
-year = int(os.getenv('YEAR', 2024))
-filename = os.getenv('FILENAME', "crawled-page-{year}.html").format(year=year)
+#public
+loc = 'C:\\Users\Gao\Documents\\assignment\week2\src\Private_Data\9999.HK_historical_data.xlsx'
 
-# get page
-page = get_url(os.getenv('URL'), filename)
+#encoding
+# with open (loc,'rb') as f:
+#     encode1 = chardet.detect(f.read())['encoding']
 
-# parse the page to html
-tree = parse(page, 'html')
+# data_location
 
-data = []
+df = pd.read_excel(loc,usecols=['Date','Close'])
+Xaxis = df['Date']
+Yaxis = df['Close']
 
-# initialize row counter
-row_num = 0
 
-for row in tree.xpath(os.getenv('ROW_XPATH')):
-    columns = row.xpath(os.getenv('COL_XPATH'))
-    columns = [column.text_content() for column in columns]
-    columns = [column.strip() for column in columns]
-    row_string = " ".join(columns).strip()
+print(Xaxis)
 
-    # skip empty rows
-    if row_string.strip() == "":
-        continue
-
-    row_num += 1
-
-    print(f'Row {row_num}: {row_string}')
-
-    month = int(columns[0])
-    day = int(columns[1])
-            
-    for i in range(2, len(columns), 2):
-        if columns[i] != "":
-            # get the time in HHMM format
-            hour = columns[i][:2]
-            minute = columns[i][2:]
-
-            dt = datetime.datetime(year,month,day,int(hour),int(minute))
-            value = columns[i+1]
-            print(f'{dt} - {value}')
-            data.append((dt, value))            
-
-# plot
-fig, ax = plt.subplots()
-ax.plot([record[0] for record in data], [float(record[1]) for record in data])
+#draw
+plt.plot(Xaxis,Yaxis)
 plt.show()
+
+
+#encoding_xls
+
+
+
+#data = []
+
