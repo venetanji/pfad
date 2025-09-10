4cdf787f657cfe677775a5e8108719ae3061bf3a
Giovanni Lion <giovanni.lion@gmail.com>
2025-09-10 13:44:14 +0800

    feat: add comprehensive examples of Python built-in data types

--- week02/scraping_utils.py
+++ week02/scraping_utils.py
@@ -6,9 +6,13 @@ import json
 
 def get_url(url, filename):
     if not os.path.exists(filename):
-
+        
+        # set useragent for requests
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        
         # fetch the page if it doesn't exist
-        page = requests.get(url)
+        page = requests.get(url, headers=headers)
 
         # save the page to a file
         with open(filename, 'w', encoding='UTF8') as f:
