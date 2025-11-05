b22d51beba8c3b92c01317dcff40732abbf64c9c
Giovanni Lion <giovanni.lion@gmail.com>
2025-11-05 22:49:07 +0800

    Refactor pdf_bot.py and qdrant_store.py for improved clarity and organization of code comments

--- week10/handbook_app/pdf_bot.py
+++ week10/handbook_app/pdf_bot.py
@@ -3,19 +3,39 @@ from langchain_classic.tools.retriever import create_retriever_tool
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 
+
+llm = ChatOpenAI(
+        # change model according to your setup
+        model="qwen3:8b",
+        # change port to 1234 for LMStudio
+        base_url="http://localhost:11434/v1",
+         # api key only needed for remote servers,
+        # otherwise ignored by local setups
+        api_key="ollama"
+    )
+
+# k is the number of relevant documents to retrieve, set to 2
 retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
 
+# the tool provided to the agent to search the student handbook
 retriever_tool = create_retriever_tool(
     retriever,
     "retrieve_from_student_handbook",
     "Retrieve information from the official polyu student handbook",
 )
 
+# tools need to be a list
 tools = [retriever_tool]
-llm = ChatOpenAI(model="qwen/qwen3-4b-2507", base_url="http://localhost:1234/v1", api_key="ollama")
+
+# create the agent
 graph = create_agent(llm, tools=tools)
 config = {"configurable": {"thread_id": "1"}}
 
+
+# simple REPL to interact with the agent
+# run python pdf_bot.py in terminal to start
+# press Ctrl+C to exit
+# Note: this is not executed when the file is imported by handbook_app.py
 if __name__ == "__main__":
     def stream_graph_updates(user_input: str):
         for event in graph.stream({"messages": [("user", user_input)]}, config=config):
--- week10/handbook_app/qdrant_store.py
+++ week10/handbook_app/qdrant_store.py
@@ -2,21 +2,23 @@ from qdrant_client.models import Distance, VectorParams
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from langchain_openai import OpenAIEmbeddings
-#from langchain_ollama import OllamaEmbeddings
-import os
-
 
 client = QdrantClient(host="localhost", port=6333)
+
 embeddings = OpenAIEmbeddings(
-    model="qwen3-embedding:0.6b", 
+    # adjust model name as needed
+    model="qwen3-embedding:0.6b",
+    # change port to 1234 for LMStudio 
     base_url="http://localhost:11434/v1", 
+    # api key only needed for remote servers, otherwise ignored by local setups
     api_key="ollama", 
     check_embedding_ctx_length=False
 )
-# embeddings = OpenAIEmbeddings()
-vector_size = len(embeddings.embed_query("sample text"))
 
+# create collection if it does not exist
 if not client.collection_exists("polyu_handbook"):
+    # get vector size from the embedding model
+    vector_size = len(embeddings.embed_query("sample text"))
     client.create_collection(
         collection_name="polyu_handbook",
         vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE)
